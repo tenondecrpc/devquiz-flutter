00418952579758f922c922c9648e7b61ba3849e7
Cristian Paniagua <tenondecrpc@gmail.com>
2021-09-26 23:00:09 -0400

    Added splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,9 @@
+import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "DevQuiz",
-        home: Scaffold(
-          appBar: AppBar(),
-        ));
+    return MaterialApp(title: "DevQuiz", home: SplashPage());
   }
 }
