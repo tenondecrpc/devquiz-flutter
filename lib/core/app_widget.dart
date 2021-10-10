9160c0baa22bb30f1573529202cd0513aea4c66d
Cristian Paniagua <tenondecrpc@gmail.com>
2021-10-09 23:15:12 -0300

    Added HomePage and ScoredCard widgets

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,9 @@
-import 'package:devquiz/splash/splash_page.dart';
+import 'package:devquiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "DevQuiz", home: SplashPage());
+    return MaterialApp(title: "DevQuiz", home: HomePage());
   }
 }
